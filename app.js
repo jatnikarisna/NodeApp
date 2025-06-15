c7bf1b0f130798b4d14af4dcda22070cba4ac8ca
jatnikarisna <jatnikarisna@gmail.com>
2025-06-15 13:10:46 +0000

    edited Jenkinsfile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share forever forever forever\n');
+  res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share #1\n');
 });
 
 app.listen(port, () => {
