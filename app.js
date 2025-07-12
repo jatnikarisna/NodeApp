1a0ffc76b49d27127d79883dc886da43b66ba444
root <root@ip-172-31-45-176.ec2.internal>
2025-07-12 14:25:56 +0000

    07122025

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share with rolling update\n');
+  res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share with rolling update 07122025\n');
 });
 
 app.listen(port, () => {
