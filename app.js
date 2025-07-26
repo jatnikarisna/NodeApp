0f9d4291189cbe40092460640145de612c265c31
root <root@ip-172-31-111-210.ec2.internal>
2025-07-26 03:45:00 +0000

    07262025-deletecreate-spirit#2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share with delete-create 07262025...spirit#1\n');
 });
 
