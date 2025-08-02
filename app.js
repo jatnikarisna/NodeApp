51db0b1911265c871e01b1bb22cbbf6d1934ce52
root <root@ip-172-31-99-31.ec2.internal>
2025-08-02 07:27:40 +0000

    08022025 - Spirit#1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share with delete-create 07262025...spirit#4\n');
+  res.send('Hello, World! Welcome Team to MOAI OSS BIG Family 2025. Let learn & share with delete-create 08022025...spirit#1\n');
 });
 
 app.listen(port, () => {
