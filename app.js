6cf3f24dcb873d271181e764934b64b8cfb75735
root <root@ip-172-31-176-45.ec2.internal>
2025-08-07 07:03:42 +0000

    delete create #1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello! Welcome Team to MOAI OSS Family 2025. Let learn Kubernetes deployment using CI/CD Jenkins with delete create method\n');
+  res.send('Hello! Welcome Team to MOAI OSS Family 2025. Let learn Kubernetes deployment using CI/CD Jenkins with delete create #1 method\n');
 });
 
 app.listen(port, () => {
